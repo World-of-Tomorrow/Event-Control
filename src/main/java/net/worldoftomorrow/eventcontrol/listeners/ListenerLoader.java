9686de9a5c95eb3d916688d79f1ce3acba98ed07
nala3 <alanjlitz@gmail.com>
2013-08-30 17:25:27 -0400

    INCOMPLETE - Push to continue work elswhere

--- src/main/java/net/worldoftomorrow/eventcontrol/listeners/ListenerLoader.java
+++ src/main/java/net/worldoftomorrow/eventcontrol/listeners/ListenerLoader.java
@@ -1,16 +1,34 @@
 package net.worldoftomorrow.eventcontrol.listeners;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
 
+import org.bukkit.event.Event;
 import org.bukkit.plugin.PluginManager;
 
 import net.worldoftomorrow.eventcontrol.EventControl;
+import net.worldoftomorrow.eventcontrol.Validate;
 
 public class ListenerLoader {
 	
+	private final EventControl instance = EventControl.instance();
 	private List<ECListener> listeners = new ArrayList<ECListener>();
+	private final File listenerFolder = new File(instance.getDataFolder() + "/listeners");
+	
+	public ListenerLoader() {
+		if(!listenerFolder.exists()) listenerFolder.mkdir();
+	}
 	
 	public void registerListeners() {
 		PluginManager pm = EventControl.instance().getServer().getPluginManager();
@@ -22,15 +40,64 @@ public class ListenerLoader {
 	
 	public void loadListeners() {
 		File lisFolder = new File(EventControl.instance().getDataFolder() + "/listeners");
-		if(!lisFolder.exists()) lisFolder.mkdir();
+		if(!Validate.isValidDir(lisFolder)) {
+			EventControl.instance().getLogger().severe("Listener folder is not valid!");
+			return;
+		}
+		List<URL> urls = new ArrayList<URL>();
+		// Get the URL for each jar containing feature(s)
+		for(File f : lisFolder.listFiles()) {
+			if(Validate.isValidFile(f) && f.getName().endsWith(".jar")) {
+				try {
+					urls.add(f.toURI().toURL());
+				} catch (MalformedURLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		URLClassLoader urlcl = new URLClassLoader((URL[]) urls.toArray());
+		Enumeration<URL> lisfiles;
+		try {
+			lisfiles = urlcl.getResources("listeners.txt");
+		} catch (IOException e) {
+			EventControl.instance().getLogger().severe("Could not load listeners files!");
+			e.printStackTrace();
+			return;
+		}
 		
+		while(lisfiles.hasMoreElements()) {
+			FileInputStream fis;
+			try {
+				fis = new FileInputStream(lisfiles.nextElement().getFile());
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+				continue;
+			}
+			BufferedReader br = new BufferedReader(new InputStreamReader(fis, Charset.forName("UTF-8")));
+			String line;
+			try {
+				while((line = br.readLine()) != null) {
+					urlcl.loadClass(line).getConstructor(Event.class, String.class);
+				}
+			} catch (IOException | ClassNotFoundException | NoSuchMethodException | SecurityException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
-	/**
-	 * We can not reload listeners, because Bukkit can not unregister
-	 * events until a restart, only loading new event listeners is possible.
-	 */
-	public void loadNewListeners() {
-		
+	public URL[] getListenerURLs() {
+		List<URL> urls =  new ArrayList<URL>();
+		for (File jar : listenerFolder.listFiles()) {
+			try {
+				if(Validate.isValidJar(jar)) {
+					urls.add(jar.toURI().toURL());
+				}
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+		}
+		return urls.toArray(new URL[urls.size()]);
 	}
+	
+	
 }
